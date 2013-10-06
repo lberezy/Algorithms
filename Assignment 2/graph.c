fbc49184ef69bf3e2e2f6f78cbfb906ac8776ceb
lucas <lucas@berezy.me>
2013-10-07 09:13:39 +1100

    Working on graph

--- Assignment 2/graph.c
+++ Assignment 2/graph.c
@@ -5,3 +5,13 @@
 
 #include <limits.h>
 
+node_t** create_graph(int N){
+	/* allocates an NxN adjacency matrix of nodes */
+	int i;
+	node_t* nodes = calloc(N * N, sizeof(node_t));
+	node_t** graph = calloc(N * sizeof(node_t*));
+	for ( i = 0; i < N; i++) {
+		graph[i] = nodes + (i * N);
+	}
+	return graph;
+}
--- Assignment 2/graph.h
+++ Assignment 2/graph.h
@@ -3,4 +3,31 @@
 	Author: Lucas Berezy
 */
 
-typedef
+
+#define blankets 32
+#define food 16
+#define water 8
+#define digging 4
+#define medicine 2
+#define noass 1
+
+/* 	information on supplies is stored in a bit-field with the following
+	format:
+	Boolean values for existance of resource.
+	XXXXX000000
+	-----------
+	XXXXXBFWDMX
+
+	– B: blankets 											(32)
+	– F: food												(16)
+	– W: water												(8)
+	– D: digging equipment 									(4)
+	– M: medicine											(2)
+	– X: no supplies or assistance available from this town	(1)
+
+	Each supply has the integer representation as above and specified in #define
+*/
+typedef struct node {
+	int time;
+	int supplies;
+}
